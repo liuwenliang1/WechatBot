8863935f12da242854967c641dce5f2e25ecf34f
chuanwu.sun <chuanwu.sun@ele.me>
2017-04-16 14:15:09 +0800

    进行了成吨的优化

--- wechatbot/bot.py
+++ wechatbot/bot.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
+import os
 import io
 import json
 import random
 import re
-import sys
 import thread
 import time
 import urllib
+import pickle
 import xml.dom.minidom
 
 import qrcode
@@ -16,18 +17,38 @@ from wechatbot.core import parse_command
 from wechatbot.exc import BotServerException, BotErrorCode
 from wechatbot.tools import create_logger
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 now = lambda: int(time.time())
 
+directory = os.path.dirname(os.path.abspath(__file__))
+PKL_FILE = '{directory}/wechat.pkl'.format(directory=directory)
 
-WECHAT_LOGIN_URL = 'https://login.weixin.qq.com/jslogin'
+WECHAT_APP_ID = 'wx782c26e4c19acffb'
+WECHAT_FIRST_LOGIN_URL = 'https://login.weixin.qq.com/jslogin'
 WECHAT_QR_CODE_STRING = 'https://login.weixin.qq.com/l/{}'
-LOGIN_TEMPLATE = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?tip=%s&uuid=%s&_=%s'
-UPLOAD_IMG = 'https://sm.ms/api/upload'
+WECHAT_SECONT_LOGIN_URL = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?tip={tip}&uuid={uuid}&_={now}'
+WECHAT_BOT_RUNNING = '机器人已经启动啦！你可以发送`ping`给机器人，机器人会回复哦。'
+WECHAT_BOT_SYNC_CHECK_URL = 'https://{sync_host}/cgi-bin/mmwebwx-bin/synccheck?{params}'
+WECHAT_BOT_SYNC_URL = '{base_uri}/webwxsync?sid={sid}&skey={skey}&lang=en_US&pass_ticket={pass_ticket}'
+WECHAT_INIT_URL = '{base_uri}/webwxinit?r={r}i&lang=en_US&pass_ticket={pass_ticket}'
 
-headers = {
+UPLOAD_IMG_URL = 'https://sm.ms/api/upload'
+
+MSG = '{color}{content}{nc}'
+
+RED = '\033[0;31m'
+GREEN = ''
+NC = '\033[0m' # No Color
+
+
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+NC = '\033[0m'  # No Color
+
+red_alert = lambda content: MSG.format(color=RED, content=content, nc=NC)
+green_alert = lambda content: MSG.format(color=GREEN, content=content, nc=NC)
+
+
+WECHAT_HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
@@ -37,70 +58,111 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
 }
 
+WECHAT_SEND_MSG_HEADER = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
+    'cache-control': 'max-age=0',
+    'content-type': 'application/json; charset=UTF-8',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+}
 
 class WechatBot(object):
 
     def __init__(self):
 
         self.session = requests.Session()
+        self.params = dict()
 
-        self.uuid = None
-        self.base_uri = None
-        self.base_host = None
+        self.params['uuid'] = None
+        self.params['base_uri'] = None
+        self.params['base_host'] = None
 
-        self.skey = None
-        self.sid = None
-        self.uin = None
-        self.pass_ticket = None
-        self.device_id = 'e' + repr(random.random())[2:17]
+        self.params['skey'] = None
+        self.params['sid'] = None
+        self.params['uin'] = None
+        self.params['pass_ticket'] = None
+        self.params['device_id'] = 'e' + repr(random.random())[2:17]
 
-        self.base_request = None
+        self.params['base_host'] = None
+        self.params['base_request'] = None
+        self.params['sync_host'] = None
+        self.params['sync_key'] = None
+        self.params['sync_key_str'] = None
 
-        self.sync_host = None
-        self.sync_key = None
-        self.sync_key_str = None
+    def read_snapshot(self):
+        with open(PKL_FILE, 'r') as fp:
+            self.params = pickle.load(fp)
 
-        self.previous_msg = ''
+    def save_snapshot(self):
+        with open(PKL_FILE, 'w') as fp:
+            pickle.dump(self.params, fp, protocol=2)
 
-    def get_uuid(self):
+    def _get_qr_code(self):
+        """
+        生成微信登录二维码
+        :return:
+        """
         params = {
-            'appid': 'wx782c26e4c19acffb',
+            'appid': WECHAT_APP_ID,
             'fun': 'new',
             'lang': 'zh_CN',
             '_': int(time.time()) * 1000 + random.randint(1, 999),
         }
-        r = self.session.get(WECHAT_LOGIN_URL, params=params)
+        r = self.session.get(WECHAT_FIRST_LOGIN_URL, params=params)
         r.encoding = 'utf-8'
         data = r.text
-        regx = r'window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)"'
-        pm = re.search(regx, data)
+        regex = r'window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)"'
+        pm = re.search(regex, data)
         if pm:
             code = pm.group(1)
-            self.uuid = pm.group(2)
-            return code == '200'
-        raise BotServerException(BotErrorCode.GET_UUID_ERROR)
+            self.params['uuid'] = pm.group(2)
+            assert code == '200'
+        else:
+            raise BotServerException(BotErrorCode.GET_UUID_ERROR)
 
-    def get_qr_code(self):
-        string = 'https://login.weixin.qq.com/l/' + self.uuid
-        img = qrcode.make(string)
+        img = qrcode.make(WECHAT_QR_CODE_STRING.format(self.params['uuid']))
         img_in_memory = io.BytesIO()
         img.save(img_in_memory, 'png')
         img_in_memory.seek(0)
         files = {'smfile': img_in_memory}
-        resp = requests.post(UPLOAD_IMG, files=files)
+        resp = requests.post(UPLOAD_IMG_URL, files=files)
         qr_code_url = json.loads(resp.content)['data']['url']
-        hint = 'open this url to scan qrcode: {} '.format(qr_code_url)
-        self.logger.info(hint)
-        return qr_code_url
+        self.logger.info(red_alert("请打开此网页并扫描二维码：{}".format(qr_code_url)))
+        img_in_memory.truncate()
 
-    def login(self):
+    def init(self):
+        """
+        初始化，在login之后来获取sync key
+        :return:
+        """
+        url = WECHAT_INIT_URL.format(base_uri=self.params['base_uri'],
+                                     r=int(time.time()),
+                                     pass_ticket=self.params['pass_ticket'])
+        params = {
+            'BaseRequest': self.params['base_request']
+        }
+        r = self.session.post(url, data=json.dumps(params))
+        r.encoding = 'utf-8'
+        dic = json.loads(r.text)
+        self.params['sync_key'] = dic['SyncKey']
+        self.params['sync_key_str'] = '|'.join([str(keyVal['Key']) + '_' + str(keyVal['Val'])
+                                                for keyVal in self.params['sync_key']['List']])
+        return dic['BaseResponse']['Ret'] == 0
+
+    def login(self, using_snap_shot=True):
+        """
+        登录
+        :return:
+        """
+        self._get_qr_code() if not using_snap_shot else self.read_snapshot()
 
         redirect_url = None
         tip = 1
 
         while not redirect_url:
-            url = LOGIN_TEMPLATE % (tip, self.uuid, now())
-            resp = self.session.get(url, headers=headers)
+            url = WECHAT_SECONT_LOGIN_URL.format(tip=tip, uuid=self.params['uuid'], now=now())
+            resp = self.session.get(url, headers=WECHAT_HEADERS)
 
             param = re.search(r'window.code=(\d+);', resp.text)
             code = param.group(1)
@@ -111,9 +173,11 @@ class WechatBot(object):
                 redirect_urls = re.search(r'\"(?P<redirect_url>.*)\"', resp.content)
                 if redirect_urls:
                     redirect_url = redirect_urls.group('redirect_url') + '&fun=new'
-                    self.base_uri = redirect_url[:redirect_url.rfind('/')]
-                    temp_host = self.base_uri[8:]
-                    self.base_host = temp_host[:temp_host.find("/")]
+                    self.params['base_uri'] = redirect_url[:redirect_url.rfind('/')]
+                    temp_host = self.params['base_uri'][8:]
+                    self.params['base_host'] = temp_host[:temp_host.find("/")]
+            elif code == '400':
+                raise BotServerException(BotErrorCode.SNAPSHOT_EXPIRED)
             else:
                 tip = 1
 
@@ -124,39 +188,24 @@ class WechatBot(object):
 
         for node in root.childNodes:
             if node.nodeName == 'skey':
-                self.skey = node.childNodes[0].data
+                self.params['skey'] = node.childNodes[0].data
             elif node.nodeName == 'wxsid':
-                self.sid = node.childNodes[0].data
+                self.params['sid'] = node.childNodes[0].data
             elif node.nodeName == 'wxuin':
-                self.uin = node.childNodes[0].data
+                self.params['uin'] = node.childNodes[0].data
             elif node.nodeName == 'pass_ticket':
-                self.pass_ticket = node.childNodes[0].data
-        if all([self.skey, self.sid, self.uin, self.pass_ticket]):
-            self.logger.info('bot is running...')
+                self.params['pass_ticket'] = node.childNodes[0].data
+        if all([self.params['skey'], self.params['sid'], self.params['uin'], self.params['pass_ticket']]):
+            self.params['base_request'] = {
+                'DeviceID': self.params['device_id'],
+                'Sid': self.params['sid'],
+                'Skey': self.params['skey'],
+                'Uin': self.params['uin']
+            }
+            self.init()
+            self.save_snapshot()
             return True
-        raise
-
-    def init(self):
-
-        url = self.base_uri + '/webwxinit?r=%i&lang=en_US&pass_ticket=%s' % (now(), self.pass_ticket)
- 
-        self.base_request = {
-            'Uin': int(self.uin),
-            'Sid': self.sid,
-            'Skey': self.skey,
-            'DeviceID': self.device_id,
-        }
- 
-        params = {
-            'BaseRequest': self.base_request
-        }
-        r = self.session.post(url, data=json.dumps(params))
-        dic = json.loads(r.text.encode('utf-8'))
-
-        self.sync_key = dic['SyncKey']
-        self.sync_key_str = '|'.join(str(data['Key']) + '_' + str(data['Val']) for data in self.sync_key['List'])
-        if dic['BaseResponse']['Ret'] != 0:
-            raise
+        raise BotServerException(BotErrorCode.LOGIN_ERROR)
 
     def sync_check(self):
         """
@@ -165,15 +214,16 @@ class WechatBot(object):
 
         params = {
             'r': now(),
-            'skey': self.skey,
-            'sid': self.sid,
-            'uin': self.uin,
-            'deviceid': self.device_id,
-            'synckey': self.sync_key_str,
+            'skey': self.params['skey'],
+            'sid': self.params['sid'],
+            'uin': self.params['uin'],
+            'deviceid': self.params['device_id'],
+            'synckey': self.params['sync_key_str'],
             '_': now()
         }
 
-        url = 'https://' + self.sync_host + '/cgi-bin/mmwebwx-bin/synccheck?' + urllib.urlencode(params)
+        url = WECHAT_BOT_SYNC_CHECK_URL.format(sync_host=self.params['sync_host'], params=urllib.urlencode(params))
+
         while True:
             try:
                 r = self.session.get(url, timeout=20)
@@ -187,24 +237,29 @@ class WechatBot(object):
                 # This is a normal response. Just ignore this exception.
                 pass
             except:
-                raise
+                raise BotServerException(BotErrorCode.SYNC_CHECK_ERROR)
 
     def sync_host_check(self):
         for host1 in ['webpush.', 'webpush2.']:
-            self.sync_host = host1 + self.base_host
+            self.params['sync_host'] = host1 + self.params['base_host']
             try:
-                retcode, selector = self.sync_check()
-                if retcode == '0':
+                code, selector = self.sync_check()
+                if code == '0':
                     return True
             except Exception as e:
                 self.logger.error(e)
-        raise
+                raise
+        raise BotServerException(BotErrorCode.SYNC_HOST_CHECK_ERROR)
 
     def sync(self):
-        url = self.base_uri + '/webwxsync?sid=%s&skey=%s&lang=en_US&pass_ticket=%s' % (self.sid, self.skey, self.pass_ticket)
+        url = WECHAT_BOT_SYNC_URL.format(base_uri=self.params['base_uri'],
+                                         sid=self.params['sid'],
+                                         skey=self.params['skey'],
+                                         pass_ticket=self.params['pass_ticket'])
+
         params = {
-            'BaseRequest': self.base_request,
-            'SyncKey': self.sync_key,
+            'BaseRequest': self.params['base_request'],
+            'SyncKey': self.params['sync_key'],
             'rr': ~int(time.time())
         }
         try:
@@ -212,13 +267,15 @@ class WechatBot(object):
             r.encoding = 'utf-8'
             dic = json.loads(r.text)
             if dic['BaseResponse']['Ret'] == 0:
-                self.sync_key = dic['SyncKey']
-                self.sync_key_str = '|'.join(str(data['Key']) + '_' + str(data['Val']) for data in self.sync_key['List'])
+                self.params['sync_key'] = dic['SyncKey']
+                self.params['sync_key_str'] = '|'.join(str(data['Key']) + '_' + str(data['Val'])
+                                                       for data in self.params['sync_key']['List'])
             return dic
         except Exception:
-            raise
+            raise BotServerException(BotErrorCode.SYNC_ERROR)
 
     def proc_msg(self):
+
         self.sync_host_check()
 
         while True:
@@ -228,22 +285,21 @@ class WechatBot(object):
             try:
                 self.handle_msg(msg)
             except Exception as e:
-                self.logger.error(e)
+                raise e
             check_time = now() - check_time
-            if check_time < 0.8:
-                time.sleep(0.8 - check_time)
+            if check_time < 1:
+                time.sleep(1 - check_time)
+            self.save_snapshot()
 
     def handle_msg(self, msgs):
-        """
-        """
         for msg in msgs["AddMsgList"]:
-            # support group message temporally
             if ':<br/>!' in msg['Content']:
                 _, msg['Content'] = msg['Content'].split('<br/>', 1)
+            self.logger.info(green_alert(msg['Content']))
             if not msg['Content'] or not msg['Content'].startswith('!'):
                 continue
             reply = {
-                'BaseRequest': self.base_request,
+                'BaseRequest': self.params['base_request'],
                 'Msg': {
                     'Type': 1,
                     'Content': parse_command(msg['Content']),
@@ -259,23 +315,15 @@ class WechatBot(object):
                 self.send_msg(reply)
 
     def send_msg(self, reply):
-        url = self.base_uri + '/webwxsendmsg'
+        url = self.params['base_uri'] + '/webwxsendmsg'
         msg_id = str(now() * 1000) + str(random.random())[:5].replace('.', '')
         reply['Msg'].update({'LocalId': msg_id, 'ClientMsgId': msg_id})
         data = json.dumps(reply, ensure_ascii=False).encode('utf-8')
         # i haven't figured out, but you have to do this...
         # i assume, the host option is used to route. So, you have to remove host before post request
-        headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-            'cache-control': 'max-age=0',
-            'content-type': 'application/json; charset=UTF-8',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-        }
         for i in range(5):
             try:
-                r = self.session.post(url, data=data, headers=headers)
+                r = self.session.post(url, data=data, headers=WECHAT_SEND_MSG_HEADER)
             except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
                 pass
             if r.status_code == 200:
@@ -289,6 +337,19 @@ class WechatBot(object):
         with thread.allocate_lock():
             return create_logger(self.__class__.__name__)
 
+    def run(self):
+        try:
+            self.login(using_snap_shot=True)
+        except Exception as e:
+            self.login(using_snap_shot=False)
+        self.logger.info(green_alert(WECHAT_BOT_RUNNING))
+        self.proc_msg()
+
 
 def init():
     return WechatBot()
+
+
+if __name__ == '__main__':
+    wechat_bot = init()
+    wechat_bot.run()
--- wechatbot/exc.py
+++ wechatbot/exc.py
@@ -15,20 +15,22 @@ class BotErrorCode(Enum):
     SYNC_CHECK_ERROR = 8
     SYNC_HOST_CHECK_ERROR = 9
     BOT_INIT_ERROR = 10
+    SNAPSHOT_EXPIRED = 11
 
 
 TRANSLATIONS = {
-    BotErrorCode.MISSING_PARAMETER: u'you may omit some parameters',
-    BotErrorCode.UNKNOWN_COMMAND: u'sorry, i can not understand.',
-    BotErrorCode.RESERVED_COMMAND: u'sorry, this command is a reserved keyword.',
-    BotErrorCode.DUPLICATED_COMMAND: u'duplicate commands.',
-    BotErrorCode.SEND_MSG_ERROR: u'failed to send msg',
-    BotErrorCode.LOGIN_ERROR: u'failed to login',
-    BotErrorCode.GET_UUID_ERROR: u'failed to get uuid',
-    BotErrorCode.SYNC_CHECK_ERROR: u'failed to sync check',
-    BotErrorCode.SYNC_HOST_CHECK_ERROR: u'failed to sync host check',
-    BotErrorCode.BOT_INIT_ERROR: u'failed to init bot',
-    BotErrorCode.SYNC_ERROR: u'failed to sync'
+    BotErrorCode.MISSING_PARAMETER: '参数不全',
+    BotErrorCode.UNKNOWN_COMMAND: '对不起，您的命令不能识别。',
+    BotErrorCode.RESERVED_COMMAND: '对不起，这是一个保留关键字。',
+    BotErrorCode.DUPLICATED_COMMAND: '已经存在相同命令',
+    BotErrorCode.SEND_MSG_ERROR: '发送消息失败',
+    BotErrorCode.LOGIN_ERROR: '登录失败',
+    BotErrorCode.GET_UUID_ERROR: '获取uuid失败',
+    BotErrorCode.SYNC_CHECK_ERROR: '同步检查失败',
+    BotErrorCode.SYNC_HOST_CHECK_ERROR: '同步HOST失败',
+    BotErrorCode.BOT_INIT_ERROR: '初始化失败',
+    BotErrorCode.SYNC_ERROR: '同步失败',
+    BotErrorCode.SNAPSHOT_EXPIRED: '对话已经过期，需要重新登录'
 }
 
 
