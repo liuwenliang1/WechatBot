c721911502b37b27b9da41941d015464ab4a470c
chuanwu sun <chuanwusun@gmail.com>
2017-12-15 19:37:41 +0800

    移除单独发消息功能

--- wechatbot/run.py
+++ wechatbot/run.py
@@ -1,11 +1,7 @@
 # -*- coding:utf-8 -*-
 import time
-import Queue
-
-import gevent
 
 from tools import now
-from wechatbot import q
 from ping import MyBot
 
 bot = MyBot()
@@ -22,22 +18,9 @@ def worker():
         if check_time < 1:
             time.sleep(1 - check_time)
             bot.save_snapshot()
-            gevent.sleep(0)
         continue
-    gevent.sleep(0)
-
-
-def chat():
-    while True:
-        try:
-            bot.send_msg_to_friend("hello", u"yo")
-            # msg = q.get(timeout=1)
-            # content, username = msg.split("$send_to$")
-            # bot.send_msg_to_friend(content, username)
-        except Queue.Empty:
-            gevent.sleep(0)
 
 
 if __name__ == '__main__':
     bot.login_wechat()
-    gevent.joinall([gevent.spawn(worker), gevent.spawn(chat)])
+    worker()
